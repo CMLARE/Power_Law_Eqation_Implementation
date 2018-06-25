8e44333660b3956025cfb0b229d1fc6a0afd1cc4
poorna14 <poorna.14@cse.mrt.ac.lk>
2018-06-25 19:10:54 +0530

    a new csv file is created after completed preprocessing

--- 1_Preprocessing.py
+++ 1_Preprocessing.py
@@ -1,27 +1,28 @@
 import csv
 
-#Maximum and minimim allowed microwave frequency
+# Maximum and minimim allowed microwave frequency
 MaxFrequency = 40.5
 MinFrequency = 12.5
 
-dataset= list(csv.reader(open('dataset.csv')))
+with open('dataset.csv'.'r') as file:
+    dataset = list(csv.reader(file))
 
-feature_arr=dataset[0]
-data_arr=[]
+    feature_arr = dataset[0]
+    data_arr = []
 
-for row_num in range(2,len(dataset)):
-    data_arr.append(dataset[row_num])
+    for row_num in range(2, len(dataset)):
+        data_arr.append(dataset[row_num])
 
+    # check whether the frequency is in the given range
+    freq_index = feature_arr.index("Frequency")
+    for row in range(0, len(data_arr)):
+        selected_link_frequency = data_arr[row][freq_index]
+        if not (MinFrequency <= float(selected_link_frequency) <= MaxFrequency):
+            del data_arr[row]
+            continue
 
-#check whether the frequency is in the given range
-freq_index=feature_arr.index("Frequency")
-for row in range(0,len(data_arr)):
-    selected_link_frequency = data_arr[row][freq_index]
-    if not( MinFrequency <= float(selected_link_frequency) <= MaxFrequency ):
-        del data_arr[row]
-        continue
-        
-print(freq_index)
+arr = feature_arr + data_arr
 
-
-
+with open('filtered_dataset.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(arr)
